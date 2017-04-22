886e24316728ee422480533c358466147e679f24
KimiaBashiran <kimia.bashiran@gmail.com>
2017-04-22 14:06:31 +0430

    Add files via upload

--- unilearn/js/Kimia.js
+++ unilearn/js/Kimia.js
@@ -16,42 +16,38 @@ function ellipsizeTextBox(id) {
 }
 
 ellipsizeTextBox("description");
-
-$(document).ready(function() {
-    $("description").dotdotdot({
-        //	configuration goes here
-    });
-});
-
 // end of ellipsis
 
 
-// Get the modal
-var modal = document.getElementById('myModal');
+// message modal
+var modal = document.getElementsByClassName('myModal');
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn = document.getElementsByClassName("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close");
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
+for (var i = 0, len = btn.length; i < len; i++) {
+    btn[i].onclick = function (event) {
+        var source = event.target || event.srcElement;
+        $("#"+$(source).attr('modal-target')).css('display','block');
+
+    };
+
+    span[i].onclick = function () {
+        for (var k = 0, len3 = modal.length; k < len3; k++) {
+            modal[k].style.display = "none";
+        }
+    };
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    for (var i = 0, len = modal.length; i < len; i++) {
+        if (event.target == modal[i]) {
+            modal[i].style.display = "none";
+        }
     }
-}
-
+};
+// end of message modal
 
 
 
